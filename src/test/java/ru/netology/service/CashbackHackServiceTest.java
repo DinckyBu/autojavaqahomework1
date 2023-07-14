5338df6acdc4c757155f02aa7d600f26adbef7c5
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-07-14 21:30:48 +0300

    Added project

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 3200;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
