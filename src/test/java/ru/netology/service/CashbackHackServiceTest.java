91d76a4848684067e063401336c48fe8f638c757
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-07-16 13:11:10 +0300

    Changed project jUnit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,9 +11,22 @@ class CashbackHackServiceTest {
 
         int amount = 3000;
 
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void remainCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 3200;
+
+        int expected = 800;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
