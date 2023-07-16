18dedc30c05d2e195efdcce6feef21ce99ac1ffa
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-07-16 13:03:21 +0300

    Changed junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,31 @@
 package ru.netology.service;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CashbackHackServiceTest {
 
     @Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
 
-        int amount = 3000;
+        int amount = 1000;
 
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
 
+        Assertions.assertEquals(expected, actual);
+    }
 
-        org.junit.Assert.assertEquals(expected, actual);
+    @Test
+    public void remainCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
