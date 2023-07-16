65b22cefa86c55fc4e30e9014d374bcb272d9487
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-07-16 13:24:35 +0300

    Changed project JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,14 +1,26 @@
 package ru.netology.service;
-import org.junit.Test;
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 3000;
 
-        int expected = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void remainCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 3200;
+
+        int expected = 800;
         int actual = service.remain(amount);
 
 
